5a159be3fee73c1a36278a368b90ac24001dc26b
taraniselsu <taraniselsu@gmail.com>
2013-04-22 00:11:30 -0700

    Missed the Readme & License files when packaging it up.

--- package.bat
+++ package.bat
@@ -6,6 +6,8 @@ mkdir Release\%DIR%
 
 xcopy /s /f /y Parts Release\%DIR%\Parts\
 xcopy /s /f /y Plugins Release\%DIR%\Plugins\
+copy /y LICENSE.txt Release\%DIR%\
+copy /y Readme.txt Release\%DIR%\
 
 cd Release
 7z a -tzip %DIR%.zip %DIR%
